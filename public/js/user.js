a75d680a98bc38be2b5a42fcb036be3b35fa6d45
Tom Hughes <tomkhughes@g.cofc.edu>
2018-03-07 14:38:45 -0800

    user.js up to date now

--- public/js/user.js
+++ public/js/user.js
@@ -1,76 +1,11 @@
+'use strict';
 $(document).ready(function () {
 
   let currentQuery;
   let currentMarket;
 
-  $('#submit').on('click', function () {
-    const input = $('.search-input').val().trim();
-    console.log(input);
-    /* zipAddress(input); */
-    apiQuery(input);
-  });
 
-  // GOOGLE MAPS 
-  var geocoder; //To use later
-  var map; //Your map
-  function initMap() {
-    geocoder = new google.maps.Geocoder();
-    //Default setup
-    var latlng = new google.maps.LatLng(32.7157, -117.1611);
-    var myOptions = {
-      zoom: 8,
-      center: latlng,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-    map = new google.maps.Map(document.getElementById("map"), myOptions);
-  }
-
-  // ------------------------------------------- Call this wherever needed to actually handle the display
-  function zipAddress(zipCode) {
-
-    var contentString = '<div id="content">' +
-      '<div id="siteNotice">' +
-      '</div>' +
-      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
-      '<div id="bodyContent">' +
-      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      'sandstone rock formation in the southern part of the ' +
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
-      'south west of the nearest large town, Alice Springs; 450&#160;km ' +
-      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
-      'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
-      'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
-      'Aboriginal people of the area. It has many springs, waterholes, ' +
-      'rock caves and ancient paintings. Uluru is listed as a World ' +
-      'Heritage Site.</p>' +
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-      'https://en.wikipedia.org/w/index.php?title=Uluru</a> ' +
-      '(last visited June 22, 2009).</p>' +
-      '</div>' +
-      '</div>';
-
-    geocoder.geocode({
-      'address': zipCode
-    }, function (results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        //Got result, center the map and put it out there
-        map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-          map: map,
-          position: results[0].geometry.location
-        });
-        var infowindow = new google.maps.InfoWindow({
-          content: contentString
-        });
-        marker.addListener('click', function () {
-          infowindow.open(map, marker);
-        });
-
-      } else {
-        alert("Geocode was not successful for the following reason: " + status);
-      }
-    });
-  }
+// ----------------------------------------- function that makes the inital farmer bureau API call
 
   function apiQuery(input) {
 
@@ -85,6 +20,16 @@ $(document).ready(function () {
 
   }
 
+ // --------------------------------------- function gets user input to update mysql table with new market info
+
+  function userInput(id, param, input) {
+    $.post(`/api/${id}/${param}/${input}`, function (body, response, err) {
+      console.log('created');
+    });
+  }
+
+// ---------------------------------------- function that generates li elements with db data and appends them to the corresponding DOM container
+
   function listGenerator(arrayOfMarkets) {
     let listContainer = $('#queryList');
     let list = arrayOfMarkets.map(function(marketData){return marketData});
@@ -95,10 +40,36 @@ $(document).ready(function () {
     
   }
 
+// --------------------------------------- function that splits the products string into an array and generates an li's, then appends them the corresponding DOM container
+
+  function productSplit(productsString){
+    let productContainer = $('#products');
+    let productArray = productsString.split(';');
+
+    for (var i = 0; i < productArray.length; i++){
+      productContainer.append(`<li>${productArray[i]}</li>`);
+    }
+  }
+// -------------------------------------- function that generates the all market information from db and appends it to the corresponding DOM container
+
   function marketInfoGenerator(marketDataObject) {
     $('#dataContainer').empty();
     const marketObject = marketDataObject;
-    console.log('RIGHT HERE!!!!!! --------> ', marketObject);
+    
+    if(marketObject.outdoors === null) {
+      marketObject.outdoors = 'N/A';
+    }
+    if (marketObject.restroom === null) {
+      marketObject.restroom = 'N/A';
+    }
+    if (marketObject.alcohol === null) {
+      marketDataObject.alcohol = "N/A"
+    }
+    if (marketObject.petFriendly === null) {
+      marketDataObject.petFriendly = "N/A"
+    }
+
+   
     let dataContainer = $('dataContainer');
     let marketContent = 
     `<div class="info">
@@ -112,11 +83,9 @@ $(document).ready(function () {
       <ul class="infoList">
         <li><span class="text-center" id="address">Address: </span><span>${marketObject.Address}</span></li>
         <li id="scheduleStyle"><span class="text-center" id="schedule">Schedule: </span><span>${marketObject.Schedule}</span></li>
-        <li><span class="text-center" id="products">Products: </span><span>${marketObject.Products}</span></li>
+        <li><span class="text-center" id="products">Products: </li>
       </ul>
      </div>
-  
-    
     <div class="amenitiesContainer">
     <ul id="amentities">
     <li>${marketObject.outdoors} <input type="text" class="input-val" id=outdoors name="input"><button class=input-submit data-name="outdoors" data-id="${marketObject.usda_id}" type="submit">Submit</button></li> 
@@ -126,24 +95,37 @@ $(document).ready(function () {
     </ul>
     </div>
     <iframe src="${marketObject.GoogleLink}" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-
-    </div>
-    </div>
   `;
-   // <iframe src="${marketObject.GoogleLink}" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-   //  </div>
+    
     $('#dataContainer').append(marketContent);
-  }
+
+    if (marketObject.Products !== "") {
+      productSplit(marketDataObject.Products);
+    }
+
+}
+
+// ----------------------------------------------------------- click handler for seach button
+
+  $('#submit').on('click', function () {
+    const input = $('.search-input').val().trim();
+    console.log(input);
+  
+    apiQuery(input);
+  });
+
   /* --------------------------------------------------------- Event delegation for market button links */
+
+ 
   $(document).on('click', '.list-group-item', function(event) {
-    event.preventDefault()
+    event.preventDefault();
     let clickedButtonMarketId = $(this).attr('data-marketid');
     console.log(clickedButtonMarketId)
     $.post(`/api/${clickedButtonMarketId}`)
     
     .done(function(data) {
-      console.log('call completed')
-      console.log(data)
+      console.log('call completed');
+      console.log(data);
 
       $.get(`/api/market-data/${clickedButtonMarketId}`, function (body) {
         marketInfoGenerator(body);
@@ -155,7 +137,7 @@ $(document).ready(function () {
     })
 
     .fail(function(err) {
-      console.log(err)
+      console.log(err);
     })
 
 
@@ -177,4 +159,5 @@ $(document).ready(function () {
       });
 
   });
+
 });
